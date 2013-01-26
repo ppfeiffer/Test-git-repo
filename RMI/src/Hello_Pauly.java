bcf7847270e1947c368ce18a0ea25ed6ac8d0fd2
Paul <ppfeiffer@student.tgm.ac.at>
2013-01-26 13:45:20 +0100

    kevin test

--- RMI/src/Hello_Pauly.java
+++ RMI/src/Hello_Pauly.java
@@ -7,15 +7,14 @@
  *
  */
 public class Hello_Pauly {
-<<<<<<< HEAD
+
 	public static void main(String args[]) {
-		System.out.println("HFHFHFHHFHFHFHFHFHFH");
+		System.out.println("z4rtz6456456ghdsfgfdsg");
 	}
-=======
 
 	public Hello_Pauly()
 	{
 		System.out.println("");
 	}
->>>>>>> branch 'master' of ssh://git@github.com/ppfeiffer/Test-git-repo.git
+
 }
