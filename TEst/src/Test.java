a4334469e612968abf3b609a3c7a3144fd04fff3
Paul <ppfeiffer@student.tgm.ac.at>
2013-01-26 11:46:54 +0100

    commmit that shit

---
+++ TEst/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test{
+	/**
+	 * Das ist ein einfach test, ob es funktioniert zu pushen.
+	 * 
+	 * @param args
+	 */
+	public static void main(String [] args)
+	{
+		System.out.println("Das ist ein test");
+	}
+}
