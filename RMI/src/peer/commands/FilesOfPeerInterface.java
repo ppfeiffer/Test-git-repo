9f9690d0ca4ba62b226bdd30d42d234c3373108e
Paul <ppfeiffer@student.tgm.ac.at>
2013-02-14 22:44:25 +0100

    removed project

--- RMI/src/peer/commands/FilesOfPeerInterface.java
+++
@@ -1,8 +0,0 @@
-package peer.commands;
-/**
- * Interface for the two FilesOfpeer classes.
- * 
- * @author Paul Pfeiffer-Vogl
- * @verison 22-Jan-2013
- */
-public interface FilesOfPeerInterface {}
